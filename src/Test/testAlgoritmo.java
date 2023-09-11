88cc5ca2088a6d5370435fee76dc4889a6fda20d
Bernardo <mtzsts1@gmail.com>
2023-09-11 16:35:25 -0600

    Correcciónes al algoritmo

--- src/Test/testAlgoritmo.java
+++ src/Test/testAlgoritmo.java
@@ -6,11 +6,11 @@ import logic.LogicExecuteAlgorithm;
 public class testAlgoritmo {
 
     public static void main(String[] args) {
-        int data [][] = {{1,1,0,1},{0,0,0,0},{0,1,0,1},{0,0,0,0}};
-        Coordinate start = new Coordinate(3,0);
-        Coordinate end = new Coordinate(0,3);
+        int data [][] = {{0,0,0,1,0,0,0,0},{0,0,0,0,0,0,1,0},{1,1,1,1,1,0,1,0},{0,0,0,0,0,0,1,0},{0,1,1,1,1,1,1,0},{0,0,0,0,0,0,0,1},};
+        Coordinate start = new Coordinate(5,0);
+        Coordinate end = new Coordinate(4,7);
 
-        LogicExecuteAlgorithm logicExecuteAlgorithm = new LogicExecuteAlgorithm(start,end,data,3,3);
+        LogicExecuteAlgorithm logicExecuteAlgorithm = new LogicExecuteAlgorithm(start,end,data,6,8);
         logicExecuteAlgorithm.executeAlgorithm();
         //logicExecuteAlgorithm.printOpenSet();
 
